71c8edefc3a6eb1396f09c1bf84fc3b95568cf5a
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-03-15 17:15:06 +0530

    feat: microservice arc

---
+++ go-server/main.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	YOUTUBE_API_KEY := os.Getenv("YOUTUBE_API_KEY")
+
+	app := fiber.New()
+	client := resty.New()
+
+	app.Use(logger.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Get("/spotify/playlists", func(c *fiber.Ctx) error {
+		// Extract Authorization header
+		authHeader := c.Get("Authorization")
+		log.Println(authHeader)
+		if authHeader == "" {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "No access token provided"})
+		}
+
+		// Make request to Spotify API
+		resp, err := client.R().
+			SetHeader("Authorization", authHeader).
+			Get("https://api.spotify.com/v1/me/playlists")
+
+		if err != nil {
+			log.Fatalf("Error fetching playlists:", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to fetch playlists"})
+		}
+
+		// Parse response
+		var result struct {
+			Items []struct {
+				Name   string `json:"name"`
+				PID    string `json:"id"`
+				Images []struct {
+					Height int    `json:"height"`
+					Width  int    `json:"width"`
+					URL    string `json:"url"`
+				} `json:"images"`
+				TracksLinks struct {
+					Href  string `json:"href"`
+					Total int    `json:"total"`
+				} `json:"tracks"`
+			} `json:"items"`
+		}
+
+		if err := json.Unmarshal(resp.Body(), &result); err != nil {
+			log.Println("Error parsing response:", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to parse playlists"})
+		}
+
+		// Format response
+		formattedPlaylists := make([]fiber.Map, len(result.Items))
+		for i, playlist := range result.Items {
+			formattedPlaylists[i] = fiber.Map{
+				"S_NAME":         playlist.Name,
+				"S_PID":          playlist.PID,
+				"S_IMAGES":       playlist.Images,
+				"S_TRACKS_LINKS": playlist.TracksLinks,
+			}
+		}
+
+		return c.JSON(formattedPlaylists)
+	})
+
+	//Get playlist's Tracks
+	app.Get("/spotify/playlist/:PID", func(c *fiber.Ctx) error {
+		PID := c.Params("PID")
+		authHeader := c.Get("Authorization")
+
+		if authHeader == "" {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Unauthorized"})
+		}
+
+		reqUrl := fmt.Sprintf("https://api.spotify.com/v1/playlists/%s/tracks", PID)
+		resp, err := client.R().
+			SetHeader("Authorization", authHeader).
+			Get(reqUrl)
+
+		if err != nil {
+			log.Fatalf("Error fetching playlist's tracks:", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to fetch playlist's tracks"})
+		}
+
+		var result struct {
+			Items []struct {
+				Track struct {
+					ID          string      `json:"id"`
+					Name        string      `json:"name"`
+					Artists     interface{} `json:"artists"`
+					Album       interface{} `json:"album"`
+					DurationMS  int         `json:"duration_ms"`
+					TrackNumber int         `json:"track_number"`
+				} `json:"track"`
+			} `json:"items"`
+		}
+
+		if err := json.Unmarshal(resp.Body(), &result); err != nil {
+			log.Println("Error parsing response:", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to parse playlist's tracks"})
+		}
+
+		formattedTracks := make([]fiber.Map, len(result.Items))
+		for i, track := range result.Items {
+			formattedTracks[i] = fiber.Map{
+				"S_TID":          track.Track.ID,
+				"S_NAME":         track.Track.Name,
+				"S_ARTISTS":      track.Track.Artists,
+				"S_ALBUM":        track.Track.Album,
+				"S_DURATION_MS":  track.Track.DurationMS,
+				"S_TRACK_NUMBER": track.Track.TrackNumber,
+			}
+		}
+
+		return c.JSON(formattedTracks)
+	})
+
+	app.Post("/youtube/search", func(c *fiber.Ctx) error {
+		type SearchRequest struct {
+			TrackName  string `json:"trackName"`
+			ArtistName string `json:"artistName"`
+		}
+
+		sq := new(SearchRequest)
+		if err := c.BodyParser(sq); err != nil {
+			log.Fatalf("Error parsing request body:", err)
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Failed to parse request body"})
+		}
+
+		trackName := url.QueryEscape(sq.TrackName)
+		artistName := url.QueryEscape(sq.ArtistName)
+
+		reqUrl := fmt.Sprintf("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=%s-%s&key=%s", trackName, artistName, YOUTUBE_API_KEY)
+
+		resp, err := client.R().SetHeader("Content-Type", "application/json").Get(reqUrl)
+		if err != nil {
+			log.Fatalf("Error fetching YT search results:", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to fetch playlist's tracks"})
+		}
+
+		var result struct {
+			Items []struct {
+				ID struct {
+					VideoId string `json:"videoId"`
+				} `json:"id"`
+				Snippet struct {
+					Title string `json:"title"`
+				} `json:"snippet"`
+			} `json:"items"`
+		}
+
+		if err := json.Unmarshal(resp.Body(), &result); err != nil {
+			log.Println("Error parsing response:", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to parse playlist's tracks"})
+		}
+
+		formattedYTResponse := make([]fiber.Map, len(result.Items))
+		for i, item := range result.Items {
+			formattedYTResponse[i] = fiber.Map{
+				"YT_VIDEO_ID": item.ID.VideoId,
+				"YT_TITLE":    item.Snippet.Title,
+			}
+		}
+
+		return c.JSON(formattedYTResponse)
+	})
+
+	log.Fatal(app.Listen(":8001"))
+}
--- server/index.js
+++ server/index.js
@@ -7,16 +7,11 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const session = require("express-session");
 const SpotifyWebApi = require("spotify-web-api-node");
-// const { spawn } = require("child_process");
-// const ffmpeg = require("fluent-ffmpeg");
-// const path = require("path");
-// const fs = require("fs");
-// const ytdlpRouter = require("./routes/ytDlp.route");
 
 const SpotifyApi = new SpotifyWebApi({
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    redirectUri: `${process.env.BACKEND_URL}/auth/spotify/callback`,
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: `${process.env.BACKEND_URL}/auth/spotify/callback`,
 });
 
 dotenv.config();
@@ -25,12 +20,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(
-    cors({
-        origin: `${process.env.FRONTEND_URL}`,
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    }),
+  cors({
+    origin: `${process.env.FRONTEND_URL}`,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
 );
 
 app.use(cookieParser());
@@ -38,211 +33,211 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
-    session({
-        secret: "jai shree Ram",
-        resave: false,
-        saveUninitialized: false,
-    }),
+  session({
+    secret: "jai shree Ram",
+    resave: false,
+    saveUninitialized: false,
+  }),
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.use(
-    new GoogleStrategy(
-        {
-            clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`,
-        },
-        function(accessToken, refreshToken, profile, done) {
-            return done(null, {
-                accessToken: accessToken,
-                refreshToken: refreshToken,
-                profile: profile,
-            });
-        },
-    ),
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, {
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+        profile: profile,
+      });
+    },
+  ),
 );
 
 passport.use(
-    new SpotifyStrategy(
-        {
-            clientID: process.env.SPOTIFY_CLIENT_ID,
-            clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-            callbackURL: `${process.env.BACKEND_URL}/auth/spotify/callback`,
-        },
-        function(accessToken, refreshToken, expires_in, profile, done) {
-            try {
-                return done(null, {
-                    accessToken: accessToken,
-                    refreshToken: refreshToken,
-                    profile: profile,
-                });
-            } catch (error) {
-                return done(error);
-            }
-        },
-    ),
+  new SpotifyStrategy(
+    {
+      clientID: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      callbackURL: `${process.env.BACKEND_URL}/auth/spotify/callback`,
+    },
+    function (accessToken, refreshToken, expires_in, profile, done) {
+      try {
+        return done(null, {
+          accessToken: accessToken,
+          refreshToken: refreshToken,
+          profile: profile,
+        });
+      } catch (error) {
+        return done(error);
+      }
+    },
+  ),
 );
 
-passport.serializeUser(function(user, done) {
-    done(null, user);
+passport.serializeUser(function (user, done) {
+  done(null, user);
 });
 
-passport.deserializeUser(function(obj, done) {
-    done(null, obj);
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
 });
 
 app.get("/", (req, res) => {
-    res.send("Backend running succesfully!!");
+  res.send("Backend running succesfully!!");
 });
 
 //SPOTIFY AUTHENTICATION ROUTES:
 app.get(
-    "/auth/spotify",
-    passport.authenticate("spotify", {
-        scope: [
-            "user-read-email",
-            "user-read-private",
-            "streaming",
-            "playlist-read-private",
-        ],
-        showDialog: true,
-    }),
+  "/auth/spotify",
+  passport.authenticate("spotify", {
+    scope: [
+      "user-read-email",
+      "user-read-private",
+      "streaming",
+      "playlist-read-private",
+    ],
+    showDialog: true,
+  }),
 );
 
 app.get(
-    "/auth/spotify/callback",
-    passport.authenticate("spotify", {
-        failureRedirect: `${process.env.FRONTEND_URL}`,
-    }),
-    (req, res) => {
-        res.cookie("spotifyAccessToken", req.user.accessToken, {
-            httpOnly: false,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "none",
-            domain: ".avadhootsmart.xyz",
-        });
-        res.redirect(`${process.env.FRONTEND_URL}/Home`);
-    },
+  "/auth/spotify/callback",
+  passport.authenticate("spotify", {
+    failureRedirect: `${process.env.FRONTEND_URL}`,
+  }),
+  (req, res) => {
+    res.cookie("spotifyAccessToken", req.user.accessToken, {
+      httpOnly: false,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+      domain: ".avadhootsmart.xyz",
+    });
+    res.redirect(`${process.env.FRONTEND_URL}/Home`);
+  },
 );
 
 //GOOGLE AUTHENTICATION ROUTES:
 app.get(
-    "/auth/google",
-    passport.authenticate("google", {
-        scope: ["profile", "email", "https://www.googleapis.com/auth/youtube"],
-    }),
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email", "https://www.googleapis.com/auth/youtube"],
+  }),
 );
 
 app.get(
-    "/auth/google/callback",
-    passport.authenticate("google", {
-        failureRedirect: `${process.env.FRONTEND_URL}`,
-    }),
-    (req, res) => {
-        res.cookie("googleAccessToken", req.user.accessToken, {
-            httpOnly: false,
-            secure: process.env.NODE_ENV === "production",
-        });
-        res.redirect(`${process.env.FRONTEND_URL}/Home`);
-    },
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: `${process.env.FRONTEND_URL}`,
+  }),
+  (req, res) => {
+    res.cookie("googleAccessToken", req.user.accessToken, {
+      httpOnly: false,
+      secure: process.env.NODE_ENV === "production",
+    });
+    res.redirect(`${process.env.FRONTEND_URL}/Home`);
+  },
 );
 
 //Routes for spotify api
 app.get("/spotify/playlists", async (req, res) => {
-    try {
-        const accessToken = req.headers.authorization.split(" ")[1];
-        if (!accessToken) {
-            return res.status(401).json({ error: "No access token provided" });
-        }
-
-        const response = await fetch("https://api.spotify.com/v1/me/playlists", {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-        });
-
-        if (!response.ok) {
-            throw new Error("Failed to fetch playlists");
-        }
-
-        const playlists = await response.json();
-        const { items } = playlists;
-
-        const formattedPlaylists = items.map((playlist) => ({
-            S_NAME: playlist.name,
-            S_PID: playlist.id,
-            S_IMAGES: playlist.images,
-            S_TRACKS_LINKS: playlist.tracks,
-        }));
-
-        res.json(formattedPlaylists);
-    } catch (error) {
-        console.error("Error fetching playlists:", error);
-        res.status(500).json({ error: "Failed to fetch playlists" });
+  try {
+    const accessToken = req.headers.authorization.split(" ")[1];
+    if (!accessToken) {
+      return res.status(401).json({ error: "No access token provided" });
     }
+
+    const response = await fetch("https://api.spotify.com/v1/me/playlists", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    const playlists = await response.json();
+    const { items } = playlists;
+
+    const formattedPlaylists = items.map((playlist) => ({
+      S_NAME: playlist.name,
+      S_PID: playlist.id,
+      S_IMAGES: playlist.images,
+      S_TRACKS_LINKS: playlist.tracks,
+    }));
+
+    res.json(formattedPlaylists);
+  } catch (error) {
+    console.error("Error fetching playlists:", error);
+    res.status(500).json({ error: "Failed to fetch playlists" });
+  }
 });
 
 //Gets playlist tracks
 app.get("/spotify/playlist/:PID", async (req, res) => {
-    const playlistId = req.params.PID;
-    const accessToken = req.headers.authorization.split(" ")[1];
-    const response = await fetch(
-        `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
-        {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-        },
-    );
-    const data = await response.json();
-    const { items } = data;
-    // res.json(data.items.track);
+  const playlistId = req.params.PID;
+  const accessToken = req.headers.authorization.split(" ")[1];
+  const response = await fetch(
+    `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
+  );
+  const data = await response.json();
+  const { items } = data;
+  // res.json(data.items.track);
 
-    const formattedPlaylistTracks = items.map((track) => ({
-        S_TID: track.track.id,
-        S_NAME: track.track.name,
-        S_ARTISTS: track.track.artists,
-        S_ALBUM: track.track.album,
-        S_DURATION_MS: track.track.duration_ms,
-        S_TRACK_NUMBER: track.track.track_number,
-        // YT_DATA: YOUTUBE_DATA;
-    }));
-    res.json(formattedPlaylistTracks);
+  const formattedPlaylistTracks = items.map((track) => ({
+    S_TID: track.track.id,
+    S_NAME: track.track.name,
+    S_ARTISTS: track.track.artists,
+    S_ALBUM: track.track.album,
+    S_DURATION_MS: track.track.duration_ms,
+    S_TRACK_NUMBER: track.track.track_number,
+    // YT_DATA: YOUTUBE_DATA;
+  }));
+  res.json(formattedPlaylistTracks);
 });
 
 //Get youtube video id for track
 app.post("/youtube/search", async (req, res) => {
-    try {
-        const { trackName, artistName } = req.body;
-        const response = await fetch(
-            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=${trackName}-${artistName}&key=${process.env.YOUTUBE_API_KEY}`,
-            {
-                headers: {
-                    Accept: "application/json",
-                },
-            },
-        );
+  try {
+    const { trackName, artistName } = req.body;
+    const response = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&q=${trackName}-${artistName}&key=${process.env.YOUTUBE_API_KEY}`,
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      },
+    );
 
-        if (!response.ok) {
-            throw new Error("YouTube API request failed");
-        }
-
-        const data = await response.json();
-        const { items } = data;
-
-        const formattedYTResponse = items.map((item) => ({
-            YT_TITLE: item.snippet.title,
-            YT_VIDEO_ID: item.id.videoId,
-        }));
-
-        res.json(formattedYTResponse);
-    } catch (error) {
-        console.error("Error searching YouTube:", error);
-        res.status(500).json({ error: "Failed to search YouTube" });
+    if (!response.ok) {
+      throw new Error("YouTube API request failed");
     }
+
+    const data = await response.json();
+    const { items } = data;
+
+    const formattedYTResponse = items.map((item) => ({
+      YT_TITLE: item.snippet.title,
+      YT_VIDEO_ID: item.id.videoId,
+    }));
+
+    res.json(formattedYTResponse);
+  } catch (error) {
+    console.error("Error searching YouTube:", error);
+    res.status(500).json({ error: "Failed to search YouTube" });
+  }
 });
 
 // app.post("/youtube/createPlaylist", async (req, res) => {
@@ -273,7 +268,7 @@ app.post("/youtube/search", async (req, res) => {
 // });
 
 app.listen(PORT, () => {
-    console.log(`Server listening http://localhost:${PORT}`);
+  console.log(`Server listening http://localhost:${PORT}`);
 });
 
 // GET https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=Levels%2C%20Avicii&key=[YOUR_API_KEY] HTTP/1.1
