a4ade0d702b21cfc9fcb8631101611f4326fc7c4
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-03-23 21:46:30 +0530

    pwa: updated link for manifest.json

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,47 +3,48 @@ import "./globals.css";
 import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
-    title: "Vybe",
-    description: "Vybe To your Music",
-    keywords:
-        "Vybe, music, songs, playlists, youtube-music, spotify, ad-free music",
-    authors: [{ name: "Avadhoot Smart", url: "https://vybe.avadhootsmart.xyz" }],
-    metadataBase: new URL("https://vybe.avadhootsmart.xyz"),
+  title: "Vybe",
+  description: "Vybe To your Music",
+  keywords:
+    "Vybe, music, songs, playlists, youtube-music, spotify, ad-free music",
+  authors: [{ name: "Avadhoot Smart", url: "https://vybe.avadhootsmart.xyz" }],
+  metadataBase: new URL("https://vybe.avadhootsmart.xyz"),
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <head>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta name="robots" content="index, follow" />
-                <meta charSet="UTF-8" />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                    href="/apple-touch-icon.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="32x32"
-                    href="/favicon-32x32.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="16x16"
-                    href="/favicon-16x16.png"
-                />
-            </head>
-            <body className={`antialiased`}>
-                <main>{children}</main>
-                <Toaster />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow" />
+        <meta charSet="UTF-8" />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </head>
+      <body className={`antialiased`}>
+        <main>{children}</main>
+        <Toaster />
+      </body>
+    </html>
+  );
 }
