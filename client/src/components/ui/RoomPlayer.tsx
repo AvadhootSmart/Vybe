d083d3cd1fed56452c574a6c98e4e2835cb629fd
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-09-08 01:33:21 +0530

    test: changing ws to wss

--- client/src/components/ui/RoomPlayer.tsx
+++ client/src/components/ui/RoomPlayer.tsx
@@ -22,7 +22,7 @@ const RoomPlayer = ({
   // Connect WebSocket
   useEffect(() => {
     const ws = new WebSocket(
-      `ws://${process.env.NEXT_PUBLIC_BACKEND_HOSTNAME}/ws/${roomID}/${isHost ? "host" : "guest"}`,
+      `wss://${process.env.NEXT_PUBLIC_BACKEND_HOSTNAME}/ws/${roomID}/${isHost ? "host" : "guest"}`,
     );
     wsRef.current = ws;
 
