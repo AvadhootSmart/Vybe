d3c68bb4c8ccd03188b7633f70a8f98bad437d21
AvadhootSmart <1234avadhootsmart@gmail.com>
2025-10-24 23:33:36 +0530

    added banner

---
+++ client/src/components/banner.tsx
@@ -0,0 +1,10 @@
+export function Banner() {
+  return (
+    <div className="px-4 py-3 bg-neutral-900">
+      <p className="text-center text-sm text-white">
+        Due to the recent changes in yt-dlp's system, you wont be able to play
+        new songs, only songs played before work for now.
+      </p>
+    </div>
+  );
+}
